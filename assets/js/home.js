6d6e120244d3b2efb5d35dbd4d80542f166685d9
unknown <jahandarmv@code.edu.az>
2023-03-02 16:21:36 +0400

    edit navbar

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,12 @@
-$(function(){
-    $('.selectpicker').selectpicker();
-});
+"use strict"
+
+let excenge=document.querySelector(".usd")
+
+
+
+
+excenge.addEventListener("click",function(){
+    let eur=document.querySelector(".eur").innerText="baku"
+
+
+})
