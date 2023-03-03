46400682b3aee7bdb86eaa13bc4261ad25e1dab9
unknown <jahandarmv@code.edu.az>
2023-03-03 09:42:23 +0400

    navbar done

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,12 +1,49 @@
 "use strict"
 
-let excenge=document.querySelector(".usd")
+// // -----usd-eur--------------
+let usd=document.querySelector(".usd-frst")
+
+let eur=document.querySelector(".eur")
 
 
-
-
-excenge.addEventListener("click",function(){
-    let eur=document.querySelector(".eur").innerText="baku"
-
-
+usd.addEventListener("click",function(){
+    document.querySelector(".usd").innerText="USD"
 })
+
+eur.addEventListener("click",function(){
+    document.querySelector(".usd").innerText="EUR"
+})
+
+// ---language------------
+
+// let lng=document.querySelector(".language")
+
+// let grm=document.querySelector(".grm")
+
+// grm.addEventListener("click",function(){
+//     lng.classList.d-none
+// })
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+  
